7d18acd9f1278e68095706146f1a681605bc0d3f
Ginger-owl <r.s.uteshev@gmail.com>
2022-10-03 14:00:52 +0600

    refactor: use express for routing

--- app.js
+++ app.js
@@ -1,44 +1,27 @@
-const http = require("http");
-//const url = require("url");
-const fs = require("fs");
 
-http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile('index.html', function (err, data) {
-      if (err) throw err;
-      res.writeHead(200, {'Content-Type':'text/html'});
-      res.write(data);
-      return res.end();
-    });
-  } else if(req.url ==='/about'){
-    fs.readFile('about.html',function(err,data){
-        if (err) throw err;
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.write(data);
-        return res.end();
-    });
-  } else if(req.url === '/contact-me'){
-    fs.readFile('contact-me.html',function(err,data){
-        if (err) throw err;
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.write(data);
-        return res.end();
-    });
-  } else if (req.url.indexOf('.css') !== -1) { 
-    fs.readFile('./styles.css', function (err, data) {
-      if (err) console.log(err);
-      res.writeHead(200, {'Content-Type': 'text/css'});
-      res.write(data);
-      res.end();
-    });
-  } else{
-    fs.readFile('404.html',function(err,data){
-        if (err) throw err;
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.write(data);
-        return res.end();
-    });
-  }
-}).listen(8080, () => {
-  console.log('Listen on port 8080');
+const express = require('express');
+const app = express();
+const port = 8080
+const { readFile } = require('fs').promises;
+
+app.use(express.static('public'))
+
+app.get('/', async (req, res) => {
+  res.send( await readFile('./index.html', 'utf-8'))
+})
+
+app.get('/about', async (req, res) => {
+  res.send( await readFile('./about.html', 'utf-8'))
+})
+
+app.get('/contact-me', async (req, res) => {
+  res.send( await readFile('./contact-me.html', 'utf-8'))
+})
+
+app.use( async (request, response) => {
+  response.status(404).send( await readFile('./404.html', 'utf8'));
 });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
