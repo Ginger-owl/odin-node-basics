07cfa51094875680b44c3aae6d15f87a53b43383
Ginger-owl <r.s.uteshev@gmail.com>
2022-10-09 00:03:29 +0600

    feat: add send email functional to the contact form

--- app.js
+++ app.js
@@ -18,10 +18,14 @@ app.get('/contact-me', async (req, res) => {
   res.send( await readFile('./contact-me.html', 'utf-8'))
 })
 
+app.get('/thankyou', async (req, res) => {
+  res.send( await readFile('./thankyou.html', 'utf-8'))
+})
+
 app.use( async (request, response) => {
   response.status(404).send( await readFile('./404.html', 'utf8'));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App started listening on port ${port}`)
 })
